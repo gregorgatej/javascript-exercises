900d20eb814f0402737ca2981fcceefdd0e4e742
Gregor Gatej <gregor.gatej@gmail.com>
2025-12-04 07:25:35 +0100

    Finish exercise 9

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(startRange, endRange) {
+    console.log(Number.isInteger(startRange));
+    if (!Number.isInteger(startRange) || !Number.isInteger(endRange)) return "ERROR";
+    if (startRange < 0 || endRange < 0) return "ERROR";
+    if (startRange > endRange) {
+        let temp = endRange;
+        endRange = startRange;
+        startRange = temp;
+    }
+    let arr = [];
+    for(let i = startRange; i <= endRange; i++) {
+        arr.push(i);
+    }
+    const sum = arr.reduce((acc, currentVal) => acc + currentVal);
+    return sum;
 };
 
 // Do not edit below this line
