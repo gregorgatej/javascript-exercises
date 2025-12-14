08eeefdb851e16026aecffad3714e1ec667f1c3f
Gregor Gatej <gregor.gatej@gmail.com>
2025-12-14 18:42:42 +0100

    Solve exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,31 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const arr = string
+                    .split("")
+                    .map(char => char.toLowerCase());
+    let j = arr.length - 1;
 
+    for (let i = 0; i < arr.length; i++) {
+        if (!isAlphaNumeric(arr[i])) continue;
+
+        while (!isAlphaNumeric(arr[j])) j--;
+
+        console.log(arr[i]);
+        console.log(arr[j]);
+        console.log("");
+
+        if (!(arr[i] === arr[j])) {
+            return false;
+        }
+
+        j--;
+    }
+
+    return true;
 };
+
+function isAlphaNumeric(char) {
+    return /[a-z0-9]/.test(char);
+}
 
 // Do not edit below this line
 module.exports = palindromes;
