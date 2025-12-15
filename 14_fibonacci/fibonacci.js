e17fb5de58f29b3a863b81ae9165c266379d7ea0
Gregor Gatej <gregor.gatej@gmail.com>
2025-12-15 06:00:20 +0100

    Finish exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num < 0) return "OOPS";
+    if (typeof num !== "number") {
+        num = parseInt(num);
+    };
+    const seq = [0, 1, 1];
+    for (let i = 2; i <= num; i++) {
+        const currEl = seq[seq.length - 2] + seq[seq.length - 1];
+        seq.push(currEl);
+    }
+    return seq[num];
 };
 
 // Do not edit below this line
