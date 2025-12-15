57a401f7144b08d3bc010322305eeb6e60021883
Gregor Gatej <gregor.gatej@gmail.com>
2025-12-15 06:54:26 +0100

    Finish exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = new Date().getFullYear();
+    return oldestPerson = people.reduce((oldest, currPerson) => {
+        const ageOldest = (oldest.yearOfDeath ?? currentYear) - oldest.yearOfBirth;
+        
+        const agePerson = (currPerson.yearOfDeath ?? currentYear) - currPerson.yearOfBirth;
 
+        return ageOldest > agePerson ? oldest : currPerson;
+    });
 };
 
 // Do not edit below this line
