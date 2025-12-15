508f55211151aeb8ae417aa44b737b774a70c5c1
Gregor Gatej <gregor.gatej@gmail.com>
2025-12-15 06:07:26 +0100

    Finish exercise 15

--- 15_getTheTitles/getTheTitles.js
+++ 15_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    return books.map(book => book.title);
 
+    // Alternative solution
+    // return books.reduce((acc, value) => {
+    //     acc.push(value.title);
+    //     return acc;
+    // }, []);
 };
 
 // Do not edit below this line
